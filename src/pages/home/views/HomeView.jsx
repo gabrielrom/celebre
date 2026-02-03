83d11149e2c33e69117823a451079998aff4d19e
gabrielrom <gabrielpessoal626@gmail.com>
2026-02-03 20:30:33 -0300

    create navbar on home

--- src/pages/home/views/HomeView.jsx
+++ src/pages/home/views/HomeView.jsx
@@ -1,9 +1,20 @@
-import styles from './HomeView.module.css'
+import Navbar from '../../../components/Navbar/Navbar';
+import styles from './HomeView.module.css';
 
 export function HomeView() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Home Page</h1>
+      <header className={styles.navbarArea}>
+        <Navbar />
+      </header>
+
+      <main className={styles.contentArea}>
+        <h1 className={styles.title}>Home Page</h1>
+      </main>
+
+      <footer className={styles.footerArea}>
+        {/* Footer content will go here */}
+      </footer>
     </div>
-  )
+  );
 }
