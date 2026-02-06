ef45026351e13c682430c560ff35b23dc7218657
gabrielrom <gabrielpessoal626@gmail.com>
2026-02-05 21:21:49 -0300

    create real state section

--- src/pages/home/views/HomeView.jsx
+++ src/pages/home/views/HomeView.jsx
@@ -3,7 +3,14 @@ import BannerCarousel from '../../../components/BannerCarousel/BannerCarousel';
 import InfoCard from '../../../components/InfoCard/InfoCard';
 import bannerImage from '../../../assets/banner1.png';
 import bannerImageMobile from '../../../assets/mobile-image.png';
-import { Award, Users, Building2 } from 'lucide-react';
+import EmpreendimentoCard from '../../../components/EmpreendimentoCard/EmpreendimentoCard';
+import CelebreButton from '../../../components/CelebreButton/CelebreButton';
+import { Award, Users, Building2, Bed, Bath, Car, MoveRight } from 'lucide-react';
+import imagem1 from '../../../assets/imagem-1.jpeg';
+import imagem2 from '../../../assets/imagem-2.jpg';
+import imagem3 from '../../../assets/imagem-3.jpg';
+import imagem4 from '../../../assets/imagem-4.jpeg';
+import imagem5 from '../../../assets/imagem-5.jpeg';
 import styles from './HomeView.module.css';
 
 export function HomeView() {
@@ -12,6 +19,67 @@ export function HomeView() {
     { image: bannerImage, imageMobile: bannerImageMobile, alt: 'Banner Promocional 2' },
     { image: bannerImage, imageMobile: bannerImageMobile, alt: 'Banner Promocional 3' }
   ];
+  const realState = [
+    {
+      id: 1,
+      image: imagem1,
+      minIncome: 'R$ 3.200,00', 
+      name: 'Forte Alencar', 
+      state: 'CE', 
+      differentials: [
+        { title: '2 quartos', icon: <Bed /> },
+        { title: '1 banheiro', icon: <Bath /> },
+        { title: '1 vaga', icon: <Car /> }
+      ]
+    },
+    {
+      id: 2, 
+      image: imagem2,
+      minIncome: 'R$ 1.000,00', 
+      name: 'Bosque das Flores',
+      state: 'CE', 
+      differentials: [
+        { title: '2 quartos', icon: <Bed /> },
+        { title: '1 banheiro', icon: <Bath /> },
+      ]
+    },
+    {
+      id: 3, 
+      image: imagem3,
+      minIncome: 'R$ 1.200,00',
+      name: 'Ipanema Beach', 
+      state: 'CE', 
+      differentials: [
+        { title: '2 quartos', icon: <Bed /> },
+        { title: '1 banheiro', icon: <Bath /> },
+        { title: '1 vaga', icon: <Car /> }
+      ]
+    },
+    {
+      id: 4, 
+      image: imagem4,
+      minIncome: 'R$ 3.200,00', 
+      name: 'Ponto de Vista', 
+      state: 'CE', 
+      differentials: [
+        { title: '2 quartos', icon: <Bed /> },
+        { title: '1 banheiro', icon: <Bath /> },
+        { title: '1 vaga', icon: <Car /> }
+      ]
+    },
+    {
+      id: 5, 
+      image: imagem5,
+      minIncome: 'R$ 1.500,00',
+      name: 'Cidade Jardim',
+      state: 'CE', 
+      differentials: [
+        { title: '2 quartos', icon: <Bed /> },
+        { title: '1 banheiro', icon: <Bath /> },
+        { title: '1 vaga', icon: <Car /> }
+      ]
+    }
+  ]
 
   return (
     <div className={styles.container}>
@@ -45,6 +113,32 @@ export function HomeView() {
               subtitle="Atuação junto a grandes construtoras como MRV e Tenda, oferecendo empreendimentos confiáveis e oportunidades reais." 
             />
           </div>
+        </section>
+
+        <section className={styles.realStateSection}>
+          <h2 className={styles.realStateTitle}>Escolha onde você quer morar</h2>
+          <p className={styles.realStateSubtitle}>
+            Selecione o apartamento que você sonha viver com a sua família e conheça todos detalhes do empreendimento.
+          </p>
+          <div className={styles.realStateList}>
+            {realState.map((empreendimento) => (
+              <EmpreendimentoCard 
+                key={empreendimento.id}
+                to={`/imoveis/detalhes/${empreendimento.id}`}
+                imageSrc={empreendimento.image}
+                minIncome={empreendimento.minIncome}
+                name={empreendimento.name}
+                state={empreendimento.state}
+                differentials={empreendimento.differentials}
+              />
+            ))}
+          </div>
+          <CelebreButton 
+            title="Ver todos os imóveis"
+            icon={<MoveRight />}
+            className={styles.realStateButton}
+            onClick={() => navigate('/imoveis')}
+          />
         </section>
       </main>
 
